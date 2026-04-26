1c18cca18669f29588d383d7c4dc62328545d793
OmarAmer30 <164549781+OmarAmer30@users.noreply.github.com>
2026-04-27 02:05:43 +0300

    Add files via upload

--- Infix to Postfix Compiler.cpp
+++ Infix to Postfix Compiler.cpp
@@ -1,10 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <fstream>
+#include <cstring>
 
 using namespace std;
 
-ifstream input("D:/My College/Level 3/Semester 2/Comp 304/Code/input.txt");
-ofstream output("D:/My College/Level 3/Semester 2/Comp 304/Code/output.txt");
+ifstream input("./input.txt");
+ofstream output("./output.txt");
 
 enum Tokens {
 	NUM = 256,
@@ -12,13 +14,59 @@ enum Tokens {
 	DIV = 258,
 	MOD = 259,
 	DONE = 300,
-	NONE = -1
+	NONE = -1,
 };
+
+#define STRMAX 999
+#define SYMMAX 100
 
 int lookahead;
 int tokenval;
-string st;
 int linenum = 1;
+
+struct Entry {
+	const char *lexptr;
+	int token;
+};
+
+char lexems[STRMAX];
+int lastchar = -1;
+
+Entry symtable[SYMMAX];
+int lastindex = 0;
+
+Entry keywords[] = {
+	{"div", DIV},
+	{"mod", MOD},
+	{0, 0}
+};
+
+int lookup(const char s[]) {
+	for (int p = lastindex; p > 0; p--)
+		if (strcmp(symtable[p].lexptr, s) == 0)
+			return p;
+	return 0;	
+}
+
+int insert(const char s[], int tok) {
+	int length = strlen(s);
+
+	if (lastindex + 1 >= SYMMAX) { cout << "symbol table full\n";  exit(1); }
+	if (lastchar + length + 1 >= STRMAX) { cout << "lexemes array full\n"; exit(1); }
+
+	lastindex++;
+	symtable[lastindex].lexptr = &lexems[lastchar + 1];
+	symtable[lastindex].token = tok;
+	lastchar = lastchar + length + 1;
+	strcpy((char*)symtable[lastindex].lexptr, s);
+
+	return lastindex;
+}
+
+void init() {
+	for (Entry* p = keywords; p->token; p++)
+		insert((char *)p->lexptr, p->token);
+}
 
 int lexan() {
 	int t;
@@ -37,7 +85,7 @@ int lexan() {
 		}
 
 		if (isalpha(t)) {
-			char buffer[20];
+			char buffer[128];
 			int i = 0;
 
 			buffer[i++] = t;
@@ -46,17 +94,15 @@ int lexan() {
 			buffer[i] = '\0';
 
 			input.unget();
-			st = string(buffer);
 
-			tokenval = t;
-
-			if (st == "div" || st == "DIV") return DIV;
-			if (st == "mod" || st == "MOD") return MOD;
-
-			return ID;
+			int p = lookup(buffer);
+			if (p == 0) p = insert(buffer, ID);
+			tokenval = p;
+			return symtable[p].token;
 		}
 		if (t == EOF) return DONE;
 
+		tokenval = NONE;
 		return t;
 	}
 }
@@ -80,7 +126,7 @@ void emit(int t, int value) {
 		output << "NUM(" << value << ") ";
 		break;
 	case ID:
-		output << "ID(" << st << ") ";
+		output << "ID(" << symtable[value].lexptr << ") ";
 		break;
 	case DIV:
 		output << "DIV ";
@@ -109,7 +155,7 @@ void factor() {
 		match(NUM);
 	}
 	else if (lookahead == ID) {
-		emit(ID, 0);
+		emit(ID, tokenval);
 		match(ID);
 	}
 	else error();
@@ -121,22 +167,22 @@ void term() {
 		if (lookahead == '*') {
 			match('*');
 			factor();
-			emit('*', NULL);
+			emit('*', 0);
 		}
 		else if (lookahead == '/') {
 			match('/');
 			factor();
-			emit('/', NULL);
+			emit('/', 0);
 		}
 		else if (lookahead == DIV) {
 			match(DIV);
 			factor();
-			emit(DIV, NULL);
+			emit(DIV, 0);
 		}
 		else if (lookahead == MOD) {
 			match(MOD);
 			factor();
-			emit(MOD, NULL);
+			emit(MOD, 0);
 		}
 		else break;
 	}
@@ -148,24 +194,28 @@ void expr() {
 		if (lookahead == '+') {
 			match('+');
 			term();
-			emit('+', NULL);
+			emit('+', 0);
 		}
 		else if (lookahead == '-') {
 			match('-');
 			term();
-			emit('-', NULL);
+			emit('-', 0);
 		}
 		else break;
 	}
 }
 
-int main() {
+void parse() {
 	lookahead = lexan();
+	while (lookahead != DONE) {
+		expr();
+		match(';');
+		output << '\n';
+	}
+}
 
-	expr();
-
-	if (lookahead != DONE)
-		error();
-
+int main() {
+	init();
+	parse();
 	return 0;
 }
